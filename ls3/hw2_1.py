f926e8c32374c9dfba6d1e38a4eb09265a7157fa
ArseniyKotuz <157214002+ArseniyKotuz@users.noreply.github.com>
2024-02-17 15:33:00 +0200

    Update hw2_1.py

--- ls3/hw2_1.py
+++ ls3/hw2_1.py
@@ -1,12 +1,5 @@
-class Solution:
-    def helper(self , root , List):
-        if root == None : 
-            return 
-        self.helper(root.left , List)
-        List.append(root.val)
-        self.helper(root.right , List)
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        List = []
-        if root == None: return List
-        self.helper(root , List)
-        return List
+class Solution():
+    def inorderTraversal(self, root ):
+        if root==None:
+            return []
+        return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right)
