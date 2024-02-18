36856582ce82fa398950b7ed0988cc42284112a4
ArseniyKotuz <arsenijkotuz@gmail.com>
2024-02-18 12:03:16 +0200

    counter

--- ls5/counter/main.py
+++ ls5/counter/main.py
@@ -1,9 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
+    
     page.title = "Counter"
     page.window_width=500
-    page.window_height=650
+    page.window_height=350
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
 
     number_input = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
@@ -25,6 +26,14 @@ def main(page: ft.Page):
         number_input.value = str(int(number_input.value) + int(txt_in.value))
         page.update()
 
+    def remove_minus_click(event):
+        txt_in.value = str(int(txt_in.value) - 1)
+        page.update()
+
+    def add_plus_click(event):
+        txt_in.value = str(int(txt_in.value) + 1)
+        page.update()
+
     page.add(
         ft.Row(
             [   
@@ -37,7 +46,9 @@ def main(page: ft.Page):
         ft.Row(
             [   
                 ft.TextButton("Remove", on_click=remove_click, width=80),
-                txt_in, 
+                ft.IconButton(ft.icons.REMOVE, on_click=remove_minus_click, width=80),
+                txt_in,
+                ft.IconButton(ft.icons.ADD, on_click=add_plus_click, width=80), 
                 ft.TextButton("Add", on_click=add_click, width=80) 
             ],
             alignment=ft.MainAxisAlignment.CENTER,
